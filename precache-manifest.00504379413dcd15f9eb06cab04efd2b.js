3cc6ac9e2d6b417272faee58b21eac346790acc6
anuragzv1 <anuragsingh@Anurags-MacBook-Pro.local>
2020-08-25 00:38:47 +0530

    Updates

---
+++ precache-manifest.00504379413dcd15f9eb06cab04efd2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84bedecc95c5c01f24cd2126e3c5b03d",
+    "url": "/ecomm/index.html"
+  },
+  {
+    "revision": "4f67a824159329c13531",
+    "url": "/ecomm/static/css/main.3c32e9ca.chunk.css"
+  },
+  {
+    "revision": "e87dc08063a098734573",
+    "url": "/ecomm/static/js/2.40127d9a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ecomm/static/js/2.40127d9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f67a824159329c13531",
+    "url": "/ecomm/static/js/main.42a0cb3e.chunk.js"
+  },
+  {
+    "revision": "49fa5933499948c05c61",
+    "url": "/ecomm/static/js/runtime-main.d57f0a8d.js"
+  }
+]);
